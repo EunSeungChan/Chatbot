3f422f1d72ebe15858c16ac2f22b82fe199a10aa
EunSeungChan <desmes0016@gmail.com>
2019-08-28 15:39:30 +0900

    add lotto.py & 동행로또 API 실험

--- lotto.py
+++ lotto.py
@@ -8,12 +8,20 @@ import requests
 # requests.get('주소')
 url = 'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=873'
 response = requests.get(url)
-#print(response.text)
 res_dict = response.json()
 print(res_dict)
 print(res_dict['drwtNo1'])
-num_list=[]
-# 1등 번호 6개가 담긴 result라는 list를 출력.
-for  i in range(1,7):
-    num_list.append(res_dict[f'drwtNo{i}'])
-print(num_list)
+
+# 1등 번호 6개가 담긴 result라는 list를 출력
+
+# 해당 코드를  Flask 서버(app.py) /lotto 함수에 적용.
+
+# Bithnumb open API를 활용하여,
+# 실시간 BTC(Bitcoin) 현재가를 출력하는 crypto.py
+
+result = []
+
+for i in range(1,7):
+    result.append(res_dict[f'drwtNo{i}'])
+
+print(result)
